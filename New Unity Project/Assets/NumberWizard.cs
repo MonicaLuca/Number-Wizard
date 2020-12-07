49d86a9d70273d91ab339e18558f4e1cff0de428
Monica Lucarini <63265170+MonicaLuca@users.noreply.github.com>
2020-12-07 00:22:55 -0500

    Added Number Wizard Welcome Message

---
+++ New Unity Project/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Think of you favorite number, and we will try to guess it!");
+        Debug.Log("Do not select a number that is higher than 1000");
+        Debug.Log("Make sure your number is not lower than 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
