7ec7be331e3276c7c435350af21f8e297bbf7692
Monica Lucarini <63265170+MonicaLuca@users.noreply.github.com>
2020-12-07 00:43:15 -0500

    add key press functionality

--- New Unity Project/Assets/NumberWizard.cs
+++ New Unity Project/Assets/NumberWizard.cs
@@ -12,14 +12,32 @@ public class NumberWizard : MonoBehaviour
         int min = 1;
 
         Debug.Log("Welcome to Number Wizard!");
-        Debug.Log("Think of you favorite number, and we will try to guess it!");
+        Debug.Log("Think of your favorite number, and we will try to guess it!");
         Debug.Log("Do not select a number that is higher than " + max);
         Debug.Log("Make sure your number is not lower than " + min);
+
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Enter = Correct!");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+
+        }
         
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
+        }
+
     }
 }
