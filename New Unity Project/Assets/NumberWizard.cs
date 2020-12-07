c4c41709ad13e50b25333022fabaa68800b2aec6
Monica Lucarini <63265170+MonicaLuca@users.noreply.github.com>
2020-12-07 00:27:50 -0500

    Added min and max variables

--- New Unity Project/Assets/NumberWizard.cs
+++ New Unity Project/Assets/NumberWizard.cs
@@ -7,10 +7,14 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Think of you favorite number, and we will try to guess it!");
-        Debug.Log("Do not select a number that is higher than 1000");
-        Debug.Log("Make sure your number is not lower than 1");
+        Debug.Log("Do not select a number that is higher than " + max);
+        Debug.Log("Make sure your number is not lower than " + min);
     }
 
     // Update is called once per frame
